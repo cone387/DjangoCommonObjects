72d47d9fb1bc23f32b81261389ffc253f19b5cb8
bizhongliang <1183008540@qq.com>
2023-05-23 16:58:41 +0800

    ConfigField支持序列化datetime time类型

--- django_common_objects/fields.py
+++ django_common_objects/fields.py
@@ -1,5 +1,16 @@
 import json
 from django.db import models
+from datetime import datetime, time as dt_time
+
+
+class DatetimeJsonEncoder(json.JSONEncoder):
+
+    def default(self, o) -> str:
+        if isinstance(o, datetime):
+            return o.strftime("%Y-%m-%d %H:%M:%S")
+        if isinstance(o, dt_time):
+            return o.strftime("%H:%M:%S")
+        return super(DatetimeJsonEncoder, self).default(o)
 
 
 class CharField(models.Field):
@@ -13,22 +24,6 @@ class CharField(models.Field):
 
 
 class ConfigField(models.JSONField):
-    _default_json_encoder = json.JSONEncoder(allow_nan=False, indent=4)
 
-    # def clean(self, value, model_instance):
-    #     value = super().clean(value, model_instance)
-    #     queryset = CommonFieldConfig.objects.filter(model=model_instance.__class__._meta.label)
-    #     model_fields = {field.key: field for field in queryset}
-    #     for k, v in value.items():
-    #         model_filed = model_fields.get(k)
-    #         validator.is_not_empty(model_filed, f'字段{k}不存在')
-    #         validator.is_false(model_filed.is_required and not v, f'字段{k}不能为空')
-    #         if model_filed.type == CommonFieldConfigType.INT:
-    #             validator.is_int(f'字段{k}必须为整数')
-    #         elif model_filed.type == CommonFieldConfigType.FLOAT:
-    #             validator.is_float(f'字段{k}必须为浮点数')
-    #         elif model_filed.type == CommonFieldConfigType.LIST:
-    #             validator.is_list(f'字段{k}必须为列表')
-    #         elif model_filed.type == CommonFieldConfigType.DICT:
-    #             validator.is_dict(f'字段{k}必须为字典')
-    #     return value
+    def __init__(self, **kwargs):
+        super().__init__(encoder=DatetimeJsonEncoder, **kwargs)
